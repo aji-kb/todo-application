810c9f501aa7291aac6a8f0c69d7e288809eea4f
Ajitha Baratharajan <ajithabaratharajan@akb.lan>
2024-08-23 20:50:05 -0500

    To Do Application - Initial Code

---
+++ src/app/component/todo/addtask.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const AddTask = (props) => {
+
+    const [todoText, setTodoText] = useState('');
+
+    const btnAddClick= ()=>{
+        props.onAdd(todoText);
+    }
+
+    const todoTextChange = (e)=>{
+        setTodoText(e.target.value);
+    }
+
+    return (
+        <>
+            <div className='input-group w-50'>
+                <div className='input-group-prepend'>
+                    <span className='input-group-text'><strong>Description</strong> </span>
+                </div>
+                <input id='taskText' className='form-control' type='text' value={todoText} onChange={(e)=>{todoTextChange(e)}}></input>
+                <input id='btnAdd' type='button' onClick={btnAddClick} className='px-3 btn btn-success' value='Add'></input>
+            </div>
+        </>
+    )
+}
+
+export default AddTask;
---
+++ src/app/component/todo/todo.jsx
@@ -0,0 +1,106 @@
+import AddTask from "./addtask";
+import {useState} from 'react';
+
+
+const ToDo = ()=>{
+
+    const [addedText, setAddedText] = useState('');
+    const [todoList, setTodoList] = useState([]);
+    const [completedTodoList, setCompletedTodoList] = useState([]);
+
+    const handleAddClick = (data)=>{
+        setAddedText(data);
+        const existingTodo = todoList.slice(); //creates a shallow copy of an array into a new object
+        if(existingTodo.indexOf(data) < 0)
+            existingTodo.push(data);
+        setTodoList(existingTodo);
+    }
+
+    const completeTask = (item)=>{
+        const existingTodo = todoList.slice();
+        const existingCompletedToDo = completedTodoList.slice();
+
+        let index = existingTodo.indexOf(item);
+        if(index >= 0)
+        {
+            existingTodo.splice(index,1);
+        }
+
+        console.log(existingTodo);
+
+        if(existingCompletedToDo.indexOf(item) <0)
+            existingCompletedToDo.push(item);
+
+        console.log(existingCompletedToDo);
+
+        setTodoList(existingTodo);
+        setCompletedTodoList(existingCompletedToDo);
+
+    }
+
+    return (
+        <>
+            <div className="container">
+                <div className="row mt-5">
+                    <div className="col"><h3>To Do Application</h3></div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col"><AddTask onAdd={(data)=>{handleAddClick(data)}}></AddTask></div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col">
+                        <table className="table table-bordered">
+                            {
+                            todoList.length > 0? 
+                            <thead>
+                                <tr>
+                                    <td><strong>Action</strong></td>
+                                    <td className="text-start"><span className="px-3"><strong>Task</strong></span></td>
+                                </tr>
+                            </thead>
+                            : <div className="text-start"><em>No Tasks! Start Adding Tasks</em></div>
+                            }
+                            <tbody>
+                                {
+                                    todoList.map((item)=>{
+                                    return <tr key={item}>
+                                                <td className="col-1"><input type="checkbox" onClick={()=>completeTask(item)}></input></td>
+                                                <td className="text-start"><span className="px-3">{item}</span></td>
+                                            </tr>
+                                })}
+                            </tbody>
+                        </table>
+
+                        <div className="mt-5">
+                        { 
+                            completedTodoList.length > 0?
+                            <div>
+                            <span className="text-start"><h6><strong>Completed Tasks</strong></h6></span>
+
+                            <table className="mt-4 table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <td className="text-start"><span className="px-3"><strong>Task</strong></span></td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                        {completedTodoList.map((item) =>{
+                                            return <tr key={item}>
+                                                <td className="text-start"><span className="px-3">{item}</span></td>
+                                            </tr>
+                                        })}
+                                </tbody>
+                            </table>
+                            </div>
+                            : ''
+                        }
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ToDo;
